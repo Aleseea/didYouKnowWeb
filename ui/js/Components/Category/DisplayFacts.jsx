933174625ec5f740a23c0a798293f1dd856a74d9
Aleseea <35313458+Aleseea@users.noreply.github.com>
2020-03-30 15:43:03 -0600

    WORKING
    Can select the correct category and change categories at will.

--- ui/js/Components/Category/DisplayFacts.jsx
+++ ui/js/Components/Category/DisplayFacts.jsx
@@ -15,52 +15,84 @@ export default class DisplayFacts extends React.Component {
         super();
         this.state = {
             errors: [],
-            Category: [],
+            selectedCategory: {},
+            allCategories: [],
         }
     }
 
+    // componentDidMount() {
+    //     console.log("Component Did Update");
+    //     CategoryApi.getCategory(this.props.selectedCategory).end((err, res) => {
+    //         if (err) {
+    //             this.setState({
+    //                 error: err.message,
+    //             });
+    //             return;
+    //         }
+    //         console.log(res.body, "res.body");
+    //         this.setState({
+    //             Category: res.body,
+    //         });
+    //     });
+    // }
+
     componentDidMount() {
-        console.log("Component Did Update");
-        CategoryApi.getCategory(this.props.selectedCategory).end((err, res) => {
+        console.log("Component Did Mount");
+        CategoryApi.getCategories().end((err, res) => {
             if (err) {
                 this.setState({
                     error: err.message,
                 });
                 return;
             }
-            console.log(res.body, "res.body");
+            console.log(res.body.results, "res.body");
             this.setState({
-                Category: res.body,
+                allCategories: res.body.results,
             });
         });
     }
 
     @autobind
-    onSelectCategory(id) {
+    findCategory(id) {
+        let selectedCategory = null;
         console.log(id, "id");
+        map(this.state.allCategories, (category) => {
+            console.log(category, "category");
+            if (category.id === id){
+                console.log("match found");
+                selectedCategory = category;
+            }
+        });
+        return selectedCategory;
     }
 
 
     renderCategoryList() {
-        console.log(this.state.Category, "state");
-        if (this.state.Category.facts != null) {
-            return map(this.state.Category.facts, (fact, index) => (
+        let category = {id: null};
+        console.log(this.props.selectedCategory, "CategoryID");
+        if (this.props.selectedCategory !== category.id || !category.id) {
+               category = this.findCategory(this.props.selectedCategory);
+               console.log(category, "category");
+
+            if (category && category.facts) {
+                return map(category.facts, (fact, index) => (
                     <li
                         className="link"
                         key={fact.id}
                     >
                         {fact.fact_text}
                     </li>
-            ));
-        } else {
-            return (
-                <li>You have an empty Category list. Please add some definitions first.</li>
-            );
+                ));
+            } else {
+                return (
+                    <li>You have an empty Category list. Please add some definitions first.</li>
+                );
+            }
         }
     }
 
     render() {
-        if(this.state.Category) {
+        if(this.props.selectedCategory) {
             return(
                 <div className={styles.facts}>
                     <h2>Facts</h2>
